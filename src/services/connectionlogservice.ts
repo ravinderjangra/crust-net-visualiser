eaf533bbd7862b530dfdc1db2d00a9def29bcdd8
Ravinder Jangra <ravinderjangra@live.com>
2018-10-15 10:13:13 +0530

    ignorefields for paginate response (#23)
    * ignorefields for paginate response
    
    * ignore region, region_code, country code
    
    * Hide random ports +  6 digit logDataHasg
    
    * refactor + EDM_Random Port

--- src/services/connectionlogservice.ts
+++ src/services/connectionlogservice.ts
@@ -4,13 +4,19 @@ import ConnectionLogModel from "../models/ConnectionLog";
 
 class ConnectionLogService {
     ignoreFields: string = "-peer_requester.ip -peer_responder.ip " +
+        "-peer_requester.geo_info.region -peer_requester.geo_info.region_code -peer_requester.geo_info.country " +
         "-peer_requester.geo_info.postal -peer_requester.geo_info.timezone -peer_requester.geo_info.longitude -peer_requester.geo_info.utc_offset " +
         "-peer_requester.geo_info.latitude -peer_requester.geo_info.languages -peer_requester.geo_info.org -peer_requester.geo_info.ip -peer_requester.geo_info.city " +
         "-peer_requester.geo_info.in_eu -peer_requester.geo_info.asn -peer_requester.geo_info.country_calling_code -peer_requester.geo_info.currency " +
         "-peer_requester.geo_info.continent_code -peer_responder.geo_info.continent_code " +
         "-peer_responder.geo_info.postal -peer_responder.geo_info.timezone -peer_responder.geo_info.longitude -peer_responder.geo_info.utc_offset " +
         "-peer_responder.geo_info.latitude -peer_responder.geo_info.languages -peer_responder.geo_info.org -peer_responder.geo_info.ip -peer_responder.geo_info.city " +
+        "-peer_responder.geo_info.region -peer_responder.geo_info.region_code -peer_responder.geo_info.country " +
         "-peer_responder.geo_info.in_eu -peer_responder.geo_info.asn -peer_responder.geo_info.country_calling_code -peer_responder.geo_info.currency ";
+
+    isHairpinnedCondition: any = { "isHairpinned": { "$in": ["false", false] } };
+
+    projectOptions: any = { _id: 0, __v: 0 };
 
     insert(log: ConnectionLog): Promise<void> {
         return new Promise((resolve, reject) => {
@@ -20,7 +26,15 @@ class ConnectionLogService {
 
     list(): Promise<Array<ConnectionLog>> {
         return new Promise((resolve, reject) => {
-            ConnectionLogModel.find({ "isHairpinned": { "$in": ["false", false] } }, { _id: 0, __v: 0 }, (err: Error, logs: Array<ConnectionLog>) => err ? reject(err) : resolve(logs))
+            ConnectionLogModel.find(this.isHairpinnedCondition, this.projectOptions, (err: Error, logs: Array<ConnectionLog> = new Array) => {
+                if (err) return reject(err);
+
+                logs.forEach((log) => {
+                    log = this.finalizeLogItem(log);
+                });
+
+                resolve(logs);
+            })
                 .sort("-createdAt")
                 .select(this.ignoreFields);
         });
@@ -28,7 +42,16 @@ class ConnectionLogService {
 
     listBetweenDates(startDate: Date, endDate: Date): Promise<Array<ConnectionLog>> {
         return new Promise((resolve, reject) => {
-            ConnectionLogModel.find({ "createdAt": { "$gte": startDate, "$lt": endDate }, "isHairpinned": { "$in": ["false", false] } }, { _id: 0, __v: 0 }, (err: Error, logs: Array<ConnectionLog>) => err ? reject(err) : resolve(logs))
+            ConnectionLogModel.find({ "createdAt": { "$gte": startDate, "$lt": endDate }, "isHairpinned": { "$in": ["false", false] } },
+                this.projectOptions, (err: Error, logs: Array<ConnectionLog> = new Array) => {
+                    if (err) return reject(err);
+
+                    logs.forEach((log) => {
+                        log = this.finalizeLogItem(log);
+                    });
+
+                    resolve(logs);
+                })
                 .sort("-createdAt")
                 .select(this.ignoreFields);
         });
@@ -40,33 +63,51 @@ class ConnectionLogService {
                 skip: size * (pageNo - 1),
                 limit: size
             };
-            ConnectionLogModel.count({ "isHairpinned": { "$in": ["false", false] } }, function (err, totalCount) {
+            let totalPages = 0;
+            ConnectionLogModel.count(this.isHairpinnedCondition, function (err, totalCount) {
                 if (err) {
-                    reject(err);
+                    return reject(err);
                 }
-                const totalPages = Math.ceil(totalCount / size);
-                ConnectionLogModel.find({ "isHairpinned": { "$in": ["false", false] } }, { _id: 0, __v: 0 }, query, function (err, data) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        const res: PaginateResponse = {
-                            logs: data,
-                            totalPages: totalPages
-                        };
-                        resolve(res);
-                    }
-                });
+                totalPages = Math.ceil(totalCount / size);
             });
+
+            ConnectionLogModel.find(this.isHairpinnedCondition, this.projectOptions, query, (err: Error, data: Array<ConnectionLog> = new Array) => {
+                if (err) {
+                    return reject(err);
+                } else {
+
+                    data.forEach((item) => {
+                        item = this.finalizeLogItem(item);
+                    });
+
+                    const res: PaginateResponse = {
+                        logs: data,
+                        totalPages: totalPages
+                    };
+
+                    resolve(res);
+                }
+            }).select(this.ignoreFields);
         });
     }
 
     getPossibleDuplicate(log: ConnectionLog): Promise<Boolean> {
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
             ConnectionLogModel.count({ "logDataHash": log.logDataHash }, function (err, c) {
                 c > 0 ? resolve(true) : resolve(false);
             });
         });
+    }
 
+    finalizeLogItem(log: ConnectionLog): ConnectionLog {
+        if (typeof log.peer_requester.nat_type === "object") {
+            log.peer_requester.nat_type = "EDM_RANDOM";
+        }
+        if (typeof log.peer_responder.nat_type === "object") {
+            log.peer_responder.nat_type = "EDM_RANDOM";
+        }
+        log.logDataHash = log.logDataHash.substr(0, 6);
+        return log;
     }
 }
 
